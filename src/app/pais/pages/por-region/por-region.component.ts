7e74b4f3f2ab7aaea892c2cd4c708bf5db4d4410
ElLuchoMan <luchoman96@gmail.com>
2021-01-31 21:18:35 -0500

    Se purgan los datos tomados del API

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Pais } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`button{ margin-right:5px}`
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  hayError: boolean = false;
+   paises: Pais[]=[];
 
-  ngOnInit(): void {
+
+  constructor(private regionService: PaisService) { }
+  getClaseCSS(region: string): string {
+    return (region === this.regionActiva) ? 'btn btn-outline-primary' : 'btn btn-primary';
+  }
+  activarRegion(region: string) {
+    if(region=== this.regionActiva){return}
+    this.regionActiva = region;
+    this.paises=[];
+    this.regionService.buscarRegion(region)
+    .subscribe(paises => this.paises = paises);
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Pais } from '../interfaces/pais.interface';
@@ -10,18 +10,25 @@ import { Pais } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = 'https://restcountries.eu/rest/v2';
+  get httpParams(){
+    return new HttpParams().set('fields','fields=name;capital;alpha2Code;flag;population');
+  }
   constructor(private http: HttpClient) { }
 
   buscarPais(termino: string): Observable<Pais[]> {
     const url = `${this.apiUrl}/name/${termino}`;
-    return this.http.get<Pais[]>(url);
+    return this.http.get<Pais[]>(url,{params: this.httpParams});
   }
   buscarCiudad(termino: string): Observable<Pais[]> {
     const url = `${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Pais[]>(url);
+    return this.http.get<Pais[]>(url,{params: this.httpParams});
   }
 getPaisPorAlpha(id:string):Observable<Pais> {
   const url = `${this.apiUrl}/alpha/${id}`;
   return this.http.get<Pais>(url);
+}
+buscarRegion(region: string): Observable<Pais[]> {
+  const url = `${this.apiUrl}/region/${region}`;
+  return this.http.get<Pais[]>(url,{params: this.httpParams});
 }
 }
